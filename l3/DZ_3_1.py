df2697c586f6d17a7584be99a4a34bbadc9aaca8
ALF-26 <emanddemms@gmail.com>
2025-02-07 22:18:24 +0200

    correction error

--- l3/DZ_3_1.py
+++ l3/DZ_3_1.py
@@ -2,21 +2,21 @@ print ('Калькулятор')
 n1 = int (input('Введіть число 1: '))
 n2 = int (input('Введіть число 2: '))
 
-x = int (input('Яку операцію виконати? \n 1 Додавання \n 2 Віднімання \n 3 Ділення \n 4 Множення \n'))
+x = input ('Яку операцію виконати?, +, -, *, / ')
 
-if x == 1:
+if x == "+":
     result = n1 + n2
     addition = 'додавання'
     action = addition
-if x == 2:
+if x == "-":
     result = n1 - n2
     subtraction = 'віднімання'
     action = subtraction
-if x == 3:
+if x == "/":
     result = float(n1 / n2)
     division = 'ділення'
     action = division
-if x == 4:
+if x == "*":
     result = n1 * n2
     multiplication = 'множення'
     action = multiplication
