e3e5c2235384b1874eabd2ba186923d3fd822c34
ALF-26 <emanddemms@gmail.com>
2025-02-07 22:07:00 +0200

    correction error

--- l2/DZ_2_2.py
+++ l2/DZ_2_2.py
@@ -1,8 +1,12 @@
-n = input('введіть 5 значне число: ')
-n = int(n)
-print( 'число перевернуте:', end='')
-print(n % 10, end='')
-print(n % 100 // 10, end='')
-print(n % 1000 // 100, end='')
-print(n % 10000 // 1000, end='')
-print(n // 10000)
+num = int(input ('введіть 5-ти значне число: '))
+n1 = num % 10
+num //= 10
+n2 = num % 10
+num //= 10
+n3 = num % 10
+num //= 10
+n4 = num % 10
+num //= 10
+n5 = num % 10
+reversed_num = n1 * 10000 + n2 * 1000 + n3 * 100 + n4 * 10 + n5
+print ('перевернуте число', reversed_num)
