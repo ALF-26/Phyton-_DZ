db1f7bae9621d29d1610ef89c36eb1aaafe6175c
ALF-26 <emanddemms@gmail.com>
2025-03-20 22:51:45 +0200

    add dz_8_2

---
+++ l8/DZ_8_2.py
@@ -0,0 +1,12 @@
+import string
+
+def is_palindrome(text):
+    text = ''.join(char.lower() for char in text if
+                   char not in string.punctuation and char != ' ')
+    return text == text[::-1]
+
+assert is_palindrome('A man, a plan, a canal: Panama') is True, 'Test1'
+assert is_palindrome('0P') is False, 'Test2'
+assert is_palindrome('a.') is True, 'Test3'
+assert is_palindrome('aurora') is False, 'Test4'
+print("ОК")
