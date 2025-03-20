f267002f74a0d110c15b74cdf523cd7d775713d9
ALF-26 <emanddemms@gmail.com>
2025-03-20 22:52:34 +0200

    add dz_8_2

--- l8/DZ_8_2.py
+++ l8/DZ_8_2.py
@@ -9,4 +9,5 @@ assert is_palindrome('A man, a plan, a canal: Panama') is True, 'Test1'
 assert is_palindrome('0P') is False, 'Test2'
 assert is_palindrome('a.') is True, 'Test3'
 assert is_palindrome('aurora') is False, 'Test4'
+
 print("ОК")
